f6d8c26c3e8427a3504cb53ffeaad9fc61fb4a1e
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-06 12:39:57 +0600

    feat(payment): add payment failure page

--- src/app/failed/page.tsx
+++ src/app/failed/page.tsx
@@ -1,23 +1,25 @@
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
-import successIcon from "@/assets/payment-success.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import failedIcon from "@/assets/payment-failed.png";
 
 export default function PaymentSuccessPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Card className="p-4 w-[350px] md:w-[500px]">
         <Image
-          src={successIcon}
+          src={failedIcon}
           width={150}
           height={150}
-          alt="Payment success icon"
+          alt="Payment failed icon"
           className="m-auto w-[100px] md:w-[150px]"
         />
         <div className="text-center space-y-1">
-          <h2 className="text-lg font-medium">Your Payment Is Successful</h2>
-          <p className="text-sm text-[#1F2937]">Thank your for your payment</p>
+          <h2 className="text-lg font-medium">Your Payment Has Failed</h2>
+          <p className="text-sm text-[#1F2937]">
+            Unfortunately, we could not process your payment. Please try again.
+          </p>
           <Link href="/" className="block">
             <Button
               variant="link"
