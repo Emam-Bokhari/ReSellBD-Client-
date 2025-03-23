b5d8ec108b70b0da97447b1a85eeacd7b4bbd30f
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-23 22:05:26 +0600

    chore: change login redirect url

--- src/components/modules/Category/index.tsx
+++ src/components/modules/Category/index.tsx
@@ -4,7 +4,14 @@ import { Fragment } from "react";
 export default function Category() {
   return (
     <Fragment>
-      <Container className="mt-4"></Container>
+      <Container className="mt-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam a,
+          aperiam debitis iste reprehenderit adipisci eaque ducimus, fugiat
+          asperiores maiores, incidunt aliquid voluptatibus voluptas beatae
+          labore ipsam cum aspernatur excepturi.
+        </p>
+      </Container>
     </Fragment>
   );
 }
